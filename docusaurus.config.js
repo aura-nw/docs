4347ef438fad48a1340ea27a326c88dbd7216545
DESKTOP-A22BOPU\Nosel <itleson4793@gmail.com>
2021-12-16 15:41:36 +0700

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,10 +43,10 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Aura.network',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo1.svg',
+          src: 'img/Logo1.svg',
         },
         items: [
           {

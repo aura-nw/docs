6ef826918acf11d45c8eeecbe2da0a0430e47723
FSOFT.FPT.VN\SonLN2 <sonln2@fsoft.com.vn>
2022-03-09 11:02:25 +0700

    fix bug

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,83 +6,81 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Docs',
-  tagline: 'Aura Network',
-  url: 'https://docs.aura.network',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/Logo1.svg',
-  organizationName: 'aura-nw', // Usually your GitHub org/user name.
-  projectName: 'Documentations', // Usually your repo name.
+    title: 'Docs',
+    tagline: 'Aura Network',
+    url: 'https://docs.aura.network',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/Logo1.svg',
+    organizationName: 'aura-nw', // Usually your GitHub org/user name.
+    projectName: 'Documentations', // Usually your repo name.
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          routeBasePath:'/',
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/aura-nw/docs/edit/main/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl: 'https://github.com/aura-nw/docs/edit/main/blog/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    routeBasePath: '/',
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    editUrl: 'https://github.com/aura-nw/docs/blob/main/',
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    editUrl: 'https://github.com/aura-nw/docs/edit/main/',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
+    themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'Aura.network',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/Logo1.svg',
+        ({
+        navbar: {
+            title: 'Aura.network',
+            logo: {
+                alt: 'My Site Logo',
+                src: 'img/Logo1.svg',
+            },
+            items: [{
+                    type: 'doc',
+                    docId: 'intro',
+                    position: 'left',
+                    label: 'Docs',
+                },
+                {
+                    to: 'blog',
+                    label: 'Blog',
+                    position: 'left'
+                }
+            ],
         },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Docs',
-          },
-          {
-            to: 'blog',
-            label: 'Blog',
-            position: 'left'
-          }
-        ],
-      },
-      footer: {  
-        links: [
-          {
-            label: 'Github',
-            href: 'https://github.com/aura-nw',
-          },
-          {
-            label: 'Discord',
-            href: 'https://discord.gg/BAYUkN2hqp',
-          },
-          {
-            label: 'Twitter',
-            href: 'https://twitter.com/AuraNetworkHQ',
-          }
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Aura Network, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
+        footer: {
+            links: [{
+                    label: 'Github',
+                    href: 'https://github.com/aura-nw',
+                },
+                {
+                    label: 'Discord',
+                    href: 'https://discord.gg/BAYUkN2hqp',
+                },
+                {
+                    label: 'Twitter',
+                    href: 'https://twitter.com/AuraNetworkHQ',
+                }
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Aura Network, Inc. Built with Docusaurus.`,
+        },
+        prism: {
+            theme: lightCodeTheme,
+            darkTheme: darkCodeTheme,
+        },
     }),
 };
 

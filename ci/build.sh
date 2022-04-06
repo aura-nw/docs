2a4a73ce1972e7b30efcebc076d2f9058d1ed6fc
kienvc <vuchikien269@gmail.com>
2022-04-06 13:44:27 +0700

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -7,6 +7,12 @@ if [ ${GITHUB_REF_NAME} = "dev" ]
 then
     echo 'This is dev branch'
 fi
+if [ ${GITHUB_REF_NAME} = "main" ]
+then
+    echo 'This is main branch'
+    cp ./src/robots/robots-aura.txt ./src/robots.txt
+    cp ./src/robots/sitemap-aura.xml ./src/sitemap.xml
+fi
 #Build and push image
 docker build -t ${CONTAINER_RELEASE_IMAGE} -f Dockerfile .
 docker push ${CONTAINER_RELEASE_IMAGE}

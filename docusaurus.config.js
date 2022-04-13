e2c3c21dde4b777f1024a28f149800f3debe6a28
ChickenHarry <96409846+ChickenHarry@users.noreply.github.com>
2022-04-13 17:30:50 +0700

    test fix navbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,6 @@ const config = {
             /** @type {import('@docusaurus/preset-classic').Options} */
             ({
                 docs: {
-                    routeBasePath: '/',
                     sidebarPath: require.resolve('./sidebars.js'),
                     // Please change this to your repo.
                     editUrl: 'https://github.com/aura-nw/docs/edit/main',

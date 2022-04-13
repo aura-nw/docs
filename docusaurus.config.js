f9c35f08487bc81ecabac1503bc1c854cf189306
ChickenHarry <96409846+ChickenHarry@users.noreply.github.com>
2022-04-13 17:43:54 +0700

    Test fix navbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
     title: 'Docs',
     tagline: 'Aura Network',
     url: 'https://docs.aura.network',
-    baseUrl: '/',
+    baseUrl: '/docs/',
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
     favicon: 'img/Aura-logo-6.png',
@@ -22,7 +22,7 @@ const config = {
             /** @type {import('@docusaurus/preset-classic').Options} */
             ({
                 docs: {
-                    routeBasePath: 'docs',
+                    routeBasePath: '/',
                     sidebarPath: require.resolve('./sidebars.js'),
                     // Please change this to your repo.
                     editUrl: 'https://github.com/aura-nw/docs/edit/main',

bb60057da96d7fa68350b9d37ed64a45763bd459
imhson <phamhoangson99@gmail.com>
2023-03-21 14:48:36 +0700

    remove the

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <Layout title='Aura Network'>
             <div className='home-root'>
                 <div className='aura-intro'>
-                    <h1>WELCOME TO THE</h1>
+                    <h1>WELCOME TO</h1>
                     <p className='aura'>AURA NETWORK</p>
                     <p>
                         This is the starting point where you will learn about the Aura ecosystem. You can find advanced
